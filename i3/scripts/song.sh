0deff618ceec8566b594728cd5b03b0dda433183
Sam Martin <Sam.Martin@net2edge.com>
2017-04-19 16:29:35 +0100

    Update song.sh script to indicate paused state

--- i3/scripts/song.sh
+++ i3/scripts/song.sh
@@ -2,5 +2,10 @@
 
 track=$(playerctl metadata title)
 artist=$(playerctl metadata artist)
+play_pause=$(playerctl status)
 
-echo -e "$artist - $track"
+if [ $play_pause = "Playing" ]; then
+  echo -e "$artist - $track"
+else
+  echo -e "$play_pause"
+fi
