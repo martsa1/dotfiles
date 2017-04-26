3ce1d4784ea23b7907d1083aa6a33240b2d8b23f
Sam Martin <Sam.Martin@net2edge.com>
2017-04-26 17:50:13 +0100

    reduce the brittleness of basic installs slightly...

--- bash_installs.sh
+++ bash_installs.sh
@@ -1,3 +1,16 @@
+#!/usr/bin/bash
+
+# Python3, we always want this!
+command -v python3 >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "Couldn't find Python 3, do you wish to install it?"
+  read -p "Install Python 3 (y/n)? " continueOrNot
+  if [ $continueOrNot == "y" -o $continueOrNot == "Y" ]; then
+    sudo apt-get install python3 python3-dev
+    sudo pip3 install virtualenvwrapper
+  fi
+fi
+
 
 # Trash-CLI provides CLI wrappers around FreeDesktop's Trash system
 command -v trash >/dev/null 2>&1
@@ -15,7 +28,7 @@ command -v git >/dev/null 2>&1
 if [ $? -ne 0 ]; then
   echo "Couldn't find git, do you wish to install it?"
   read -p "Install Git (y/n)? " continueOrNot
-  if [$continueOrNot == "y" -o $continueOrNot == "Y"]; then
+  if [ $continueOrNot == "y" -o $continueOrNot == "Y" ]; then
     sudo apt-get install git
   fi
 fi
@@ -25,18 +38,7 @@ command -v autojump >/dev/null 2>&1
 if [ $? -ne 0 ]; then
   echo "Couldn't find autojump, do you wish to install it?"
   read -p "Install autojump (y/n)? " continueOrNot
-  if [$continueOrNot == "y" -o $continueOrNot == "Y"]; then
+  if [ $continueOrNot == "y" -o $continueOrNot == "Y" ]; then
     sudo apt-get install autojump
-  fi
-fi
-
-# Python3, we always want this!
-command -v python3 >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-  echo "Couldn't find Python 3, do you wish to install it?"
-  read -p "Install Python 3 (y/n)? " continueOrNot
-  if [$continueOrNot == "y" -o $continueOrNot == "Y"]; then
-    sudo apt-get install python3 python3-dev
-    sudo pip3 install virtualenvwrapper
   fi
 fi
