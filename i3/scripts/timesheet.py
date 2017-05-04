a72dd5b0e96852246cf5b0784c57bb3e23561ee6
Sam Martin <Sam.Martin@net2edge.com>
2017-05-04 19:10:01 +0100

    Fix timesheet error that randomly appeared and clean timesheet script

--- i3/scripts/timesheet.py
+++ i3/scripts/timesheet.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 
-from subprocess import check_output
+import subprocess
+# from subprocess import check_output
 
 
-def main():
+def main() -> str:
     ''' Simple process that greps the output from timetrap to return the
         current sheet and the time entry thats running on it.
     '''
     timesheet_info = get_timetrap_output()
     default_sink = timesheet_info.split('*')[-1].strip()
-    #
-    # volume = re.search(r'^\s+volume:.+/\s+(\d{1,2}%).+$',
-    #                    default_sink,
-    #                    flags=re.MULTILINE)
+
     if default_sink:
         print(default_sink)
 
@@ -20,9 +18,10 @@ def main():
 def get_timetrap_output() -> str:
     '''Grabs the output from timetrap ready for parsing
     '''
-    timesheet_info = check_output([
-        '/usr/local/bin/t',
-        'now']).decode('ascii')
+    timesheet_info = subprocess.check_output(
+        ['/usr/local/bin/t',
+         'now'],
+        stderr=subprocess.STDOUT).decode('ascii')
     return timesheet_info
 
 
