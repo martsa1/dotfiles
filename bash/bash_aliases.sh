9d37b5587f9845fccd575fc9afdb4204da8923a5
Sam Martin <Sam.Martin@Transition.com>
2018-01-25 19:00:49 +0000

    Add shortcut to check battery level

--- bash/bash_aliases.sh
+++ bash/bash_aliases.sh
@@ -32,6 +32,8 @@ alias codeterm="dbus-send --session /net/sf/roxterm/Options net.sf.roxterm.Optio
 alias ipython="ipython --TerminalInteractiveShell.editing_mode=vi"
 alias python_tags=$'ctags -R --fields=+l --languages=python --python-kinds=-iv -f ./tags $(python -c "import os, sys; print(\' \'.join(\'{}\'.format(d) for d in sys.path if os.path.isdir(d)))")'
 
+# Add an alias to use when checking battery info
+alias batinfo='upower -i /org/freedesktop/UPower/devices/battery_BAT1'
 if [ -f ~/.config/bash_work.sh ]; then
     . ~/.config/bash_work.sh
 fi
