a2901ab6285f5a13e8293d86faccc36f9daf4773
Sam Martin <Sam.Martin@Net2Edge.com>
2018-02-05 00:21:16 +0000

    Add tests for bash role -- needs a user role dependency before really coming into its own

--- roles/bash/molecule/default/tests/test_default.py
+++ roles/bash/molecule/default/tests/test_default.py
@@ -6,9 +6,21 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_bashrc(host):
+    ''' Checks that the bashrc file exists and is owned by the correct user.
+    '''
+    bashrc_file = host.file('/home/sam/.bashrc')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert bashrc_file.exists
+    # TODO: Sort out a user role before enabling these!!
+    # assert bashrc_file.user == 'sam'
+    # assert bashrc_file.group == 'sam'
+
+
+def test_bashit_installed(host):
+    ''' Checks that the bash-it command is available for use.
+    '''
+    bash_it_present = host.run(
+        "'source /home/sam/.bashrc && command -v bash-it'"
+    )
+    assert bash_it_present.rc == 0
