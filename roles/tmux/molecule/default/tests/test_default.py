dc44a88c77bd863935ebe47d0e9236c19d23a264
Sam Martin <Sam.Martin@Net2Edge.com>
2018-07-09 01:53:04 +0100

    Update Tmux molecule to emulate non-privileged test user
    - Some other tweaks to test environment
    - Add support for Arch linux, by simply installing tmux, rather than
      building from source.

--- roles/tmux/molecule/default/tests/test_default.py
+++ roles/tmux/molecule/default/tests/test_default.py
@@ -11,18 +11,16 @@ def test_bashrc(host):
     ''' Checks that the tmux config file exists and is owned by the correct
         user.
     '''
-    tmux_config = host.file('/home/sam/.tmux.conf')
-
-    assert tmux_config.exists
-    # TODO: Sort out a user role before enabling these!!
-    # assert bashrc_file.user == 'sam'
-    # assert bashrc_file.group == 'sam'
+    tmux_config = host.run(
+        "sudo ls /home/example/.tmux.conf"
+    )
+    assert tmux_config.rc == 0
 
 
-def test_bashit_installed(host):
+def test_tmux_installed(host):
     ''' Checks that tmux is available for use.
     '''
-    bash_it_present = host.run(
+    tmux_installed = host.run(
         "tmux --version"
     )
-    assert bash_it_present.rc == 0
+    assert tmux_installed.rc == 0
