64f8a8359114245712f48b951497dc3430f6a464
Sam Martin <nivekkas@gmail.com>
2018-11-08 10:30:13 +0000

    Re-enable hacky python neovim environments - Should update technique soon

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,4 +1,4 @@
-#1/usr/env sh
+#!/usr/bin/env bash
 # One-liner executable to run through the installation of Kitty on the local host.
 pushd ..
 ansible-playbook -i 'localhost,' -c local -K setup_neovim.yml
