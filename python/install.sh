ef5cdaca8469013e6e6fda129a52db364883c3d1
Sam Martin <sam@abitmoredepth.com>
2018-11-14 19:43:55 +0000

    Amend python role test and install files

--- python/install.sh
+++ python/install.sh
@@ -1,3 +1,5 @@
-#1/usr/env sh
-# One-liner executable to run through the installation of Kitty on the local host.
-ansible-playbook -i 'localhost,' -c local -K playbook.yml
+#!/bin/bash
+# One-liner executable to run through the installation of python on the local host.
+pushd ..
+ansible-playbook -i 'localhost,' -c local -K python/playbook.yml
+popd
