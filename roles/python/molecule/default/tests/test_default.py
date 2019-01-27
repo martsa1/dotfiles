d0dfa81ee76234f0b2578f58d61c2730cc909764
Sam Martin <sam@abitmoredepth.com>
2019-01-27 21:43:10 +0000

    Overhaul Python role to make everything run through pyenv

--- roles/python/molecule/default/tests/test_default.py
+++ roles/python/molecule/default/tests/test_default.py
@@ -2,10 +2,9 @@ import os
 
 import testinfra.utils.ansible_runner
 
-
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
-
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
 
 # Issues with using host.file (the normal way to test these kinds of things)
 # are due to the way permissions are setup within the test container.  Using
@@ -13,21 +12,27 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 # https://github.com/philpep/testinfra/blob/master/testinfra/modules/file.py#L42
 
 
-def test_python_installed(host):
-    assert host.package('python2.7').is_installed
-    assert host.package('python3').is_installed
-
-
 def test_pipsi_installed(host):
     with host.sudo():
-        host.run_expect([0], 'test -e /home/example/.local/bin/pipsi')
-        host.run_expect([0], 'test -L /home/example/.local/bin/pipsi')
+        host.run_expect(
+            [0],
+            'test -e /home/example/.local/bin/pipsi',
+        )
+        host.run_expect(
+            [0],
+            'test -L /home/example/.local/bin/pipsi',
+        )
 
 
 def test_glances_installed(host):
     with host.sudo():
-        glances_file = host.run('test -e /home/example/.local/bin/glances').rc
-        glances_symlink = host.run('test -L /home/example/.local/bin/glances').rc
+        glances_file = host.run(
+            'test -e /home/example/.local/bin/glances'
+        ).rc
+
+        glances_symlink = host.run(
+            'test -L /home/example/.local/bin/glances'
+        ).rc
 
     assert glances_file == 0
     assert glances_symlink == 0
@@ -35,7 +40,8 @@ def test_glances_installed(host):
 
 def test_pyenv_installed(host):
     with host.sudo():
-        pyenv_file = host.run('test -e /home/example/code/personal/pyenv/bin/pyenv').rc
+        pyenv_file = host.run(
+            'test -e /home/example/code/personal/pyenv/bin/pyenv'
+        ).rc
 
     assert pyenv_file == 0
-
