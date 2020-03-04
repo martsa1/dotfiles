3a41760efb63b78b78da55e8ee44a92bca12bb17
Sam Martin <martsa@f-secure.com>
2020-03-04 17:28:26 +0000

    Tweak i3 install command and add rofi-calc to work config

--- i3/install.sh
+++ i3/install.sh
@@ -1,3 +1,3 @@
-#1/usr/env sh
+#!/bin/bash
 # One-liner executable to run through the installation of Kitty on the local host.
-ansible-playbook -i 'localhost,' -c local -K playbook.yml
+ANSIBLE_ROLES_PATH=$(pwd)/../roles ansible-playbook -i 'localhost,' -c local -K playbook.yml
