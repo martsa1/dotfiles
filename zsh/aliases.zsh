2e4aa11d37b3951c32b68ee761cfe13d15287702
Sam Martin <Nivekkas@gmail.com>
2020-03-21 17:22:25 +0000

    Add shortcut for neovide

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -73,3 +73,7 @@ alias dchrome='docker run -it \
 # Kill all the tabs in Chrome to free up memory
 # [C] explained: http://www.commandlinefu.com/commands/view/402/exclude-grep-from-your-grepped-output-of-ps-alias-included-in-description
 alias chromekill="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs -r kill"
+
+nv () {
+  neovide $@ & disown
+}
