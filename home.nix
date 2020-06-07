306fab7314653fa3a361910765fcb543cec0f643
Sam Martin <martsa@f-secure.com>
2020-06-07 17:47:51 +0100

    Add ZPlug from nixpkgs-unstable

--- home.nix
+++ home.nix
@@ -1,6 +1,11 @@
 { pkgs, ...}:
 
 let
+  # unstable_pkgs = import <nixpkgs-unstable> {};
+  unstable_pkgs = import (
+    fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz
+  ) {};
+
   python_version = pkgs.python38;
   custom_pkgs = pkgs.callPackage ./pkgs/all.nix { inherit pkgs; python3=python_version; };
 in
@@ -36,5 +41,17 @@ in
     xorg.xev
     xorg.xprop
     yubioath-desktop
+    unstable_pkgs.zplug
+    # zsh
   ];
+
+  programs.zsh.enable = true;
+  # programs.zsh.zplug.enable = true;
+
+  # Ensure ZSH setup pulls in my dotfiles stuff...
+  programs.zsh.initExtra = ''
+    if [ -f "$HOME/code/personal/dotfiles/zsh/zshrc" ]; then
+      source "$HOME/code/personal/dotfiles/zsh/zshrc"
+    fi
+  '';
 }
