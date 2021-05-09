a1a02b5ddec5989d0cf7bb57a306df5cc945623f
Sam Martin <Nivekkas@gmail.com>
2021-05-09 13:37:58 +0100

    Fix Tmux dracula config, add other missing settings.

--- dotfiles/tmux/default.nix
+++ dotfiles/tmux/default.nix
@@ -1,34 +1,74 @@
+/* vim: set filetype=nix ts=2 sw=2 tw=0 et :*/
 { pkgs, ...}:
 
 {
 programs.tmux = {
     enable = true;
-    terminal = "screen-256color";
+    terminal = "alacritty";
     aggressiveResize = true;
     baseIndex = 1;
     clock24 = true;
     escapeTime = 10;
     keyMode = "vi";
     historyLimit = 10000;
+    extraConfig = ''
+      # Force Tmux to use 24bit colour
+      set-option -sa terminal-overrides ",alacritty*:Tc"
+
+      # Make Tmux highlight activity in other panes by defualt
+      setw -g monitor-activity on
+      # Allow swapping panes around
+      bind-key -r "<" swap-window -t -1
+      bind-key -r ">" swap-window -t +1
+
+      # Jump to last used window
+      bind-key "C-^" last-window
+      bind-key 6 select-window -t :=6
+
+      # Add key-binding to re-number windows in a tmux session
+      bind-key "W" move-window -r
+
+      # mouse control (clickable windows, resizable panes)
+      set -g mouse on
+
+      # Open new panes in current path by default
+      bind-key "c" new-window -c "#{pane_current_path}"
+
+      # reload config file
+      bind r source-file ~/.tmux.conf
+    '';
 
 
     plugins = with pkgs.tmuxPlugins; [
-      # sensible
       yank
       copycat
       resurrect
-      onedark-theme
-      # TODO - Find out why this doesn't work
-      #dracula
-      #{
-      #  plugin = dracula;
-      #  extraConfig = ''
-      #    set -g @dracula-show-battery false
-      #    set -g @dracula-show-powerline true
-      #    set -g @dracula-refresh-rate 10
-      #  '';
-      #}
+      #onedark-theme
+      {
+        plugin = dracula;
+        extraConfig = ''
+          set -g @dracula-show-battery true
+          set -g @dracula-show-network false
+          set -g @dracula-show-weather false
+          set -g @dracula-show-fahrenheit false
+          set -g @dracula-show-location false
+          set -g @dracula-show-powerline true
+          set -g @dracula-show-flags true
+          set -g @dracula-show-left-icon session
+          set -g @dracula-left-icon-padding 1
+          set -g @dracula-military-time true
+          set -g @dracula-show-timezone true
+          set -g @dracula-show-left-sep 
+          set -g @dracula-show-right-sep 
+          set -g @dracula-border-contrast true
+          set -g @dracula-cpu-usage true
+          set -g @dracula-ram-usage true
+          set -g @dracula-gpu-usage false
+          set -g @dracula-day-month false
+          set -g @dracula-show-time true
+          set -g @dracula-refresh-rate 5
+        '';
+      }
     ];
-     # extraConfig = builtins.readFile ./dotfiles/tmux/tmux.conf;
   };
 }
