687f690454efd8282271b5a099b38847b6cab782
Sam Martin <Nivekkas@gmail.com>
2021-10-24 18:39:23 +0100

    Tweak Tmux powerline config.

--- dotfiles/tmux/default.nix
+++ dotfiles/tmux/default.nix
@@ -37,12 +37,10 @@ programs.tmux = {
       # reload config file
       bind r source-file ~/.tmux.conf
 
-
       # Make windows with changes easier to notice
       set-window-option -g window-status-bell-style "reverse"
       set-window-option -g window-status-activity-style "bold"
     '';
-
 
     plugins = with pkgs.tmuxPlugins; [
       yank
@@ -51,27 +49,11 @@ programs.tmux = {
       #onedark-theme
       {
         plugin = dracula;
-        extraConfig = ''
-          set -g @dracula-show-battery true
-          set -g @dracula-show-network false
-          set -g @dracula-show-weather false
-          set -g @dracula-show-fahrenheit false
-          set -g @dracula-show-location false
-          set -g @dracula-show-powerline true
-          set -g @dracula-show-flags true
-          set -g @dracula-show-left-icon session
-          set -g @dracula-left-icon-padding 1
-          set -g @dracula-military-time true
-          set -g @dracula-show-timezone true
-          set -g @dracula-show-left-sep 
-          set -g @dracula-show-right-sep 
-          set -g @dracula-border-contrast true
-          set -g @dracula-cpu-usage true
-          set -g @dracula-ram-usage true
-          set -g @dracula-gpu-usage false
-          set -g @dracula-day-month false
-          set -g @dracula-show-time true
-          set -g @dracula-refresh-rate 5
+        extraConfig= ''
+        # available plugins: battery, cpu-usage, git, gpu-usage, ram-usage, network, network-bandwidth, weather, time
+        set -g @dracula-plugins "battery cpu-usage ram-usage time"
+
+        set -g @dracula-show-powerline true
         '';
       }
     ];
