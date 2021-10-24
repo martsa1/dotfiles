4ee97cde5245876e0b0edb7ac63826133f4cc705
Sam Martin <Nivekkas@gmail.com>
2021-10-24 18:35:59 +0100

    Add rofi-pkg to all.pkg

--- pkgs/all.nix
+++ pkgs/all.nix
@@ -7,4 +7,5 @@
   polybar-spotify = pkgs.callPackage ./polybar-spotify { inherit pkgs; inherit python3; };
   i3-config = pkgs.callPackage ./i3-config { inherit pkgs; inherit python3; };
   dunst-dracula-theme = pkgs.callPackage ./dunst-dracula-theme { inherit pkgs; };
+  rofi-dracula-theme = pkgs.callPackage ./rofi-dracula-theme {inherit pkgs; };
 }
