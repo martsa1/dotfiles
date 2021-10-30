308a729eb987c5153f2f79bace95054fad26be81
Sam Martin <Nivekkas@gmail.com>
2021-10-30 20:06:42 +0100

    Split home.nix into multiple profiles to support linux and darwin

---
+++ machines/common/default.nix
@@ -0,0 +1,118 @@
+# vim: set filetype=nix ts=2 sw=2 tw=0 et :
+{ pkgs, ...}:
+
+let
+  # unstable_pkgs = import <nixpkgs-unstable> {};
+  unstable_pkgs = import (
+    fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz
+  ) {};
+
+  python_version = pkgs.python39;
+  custom_pkgs = pkgs.callPackage ../../pkgs/all.nix { inherit pkgs; python3=python_version; };
+in
+
+{
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+    }))
+  ];
+
+  imports = [
+    # Enable and manage tmux via home-manager
+    ../../dotfiles/tmux
+  ];
+
+  # Various packages I want my user to have access to
+  home.packages = with pkgs; [
+    bat
+    custom_pkgs.rofi-dracula-theme
+    docker-compose
+    file
+    jq
+    lsd
+    meld
+    pass
+    pstree
+    python39Packages.ipython
+    python39Packages.powerline
+    rnix-lsp
+    tldr
+    universal-ctags
+    zplug
+    # unstable_pkgs.zplug
+    vifm
+
+    # Youtube stuff
+    youtube-dl
+    ffmpeg
+  ];
+
+  programs = {
+    zsh = {
+      enable = true;
+      oh-my-zsh.enable = false;
+
+      # Ensure ZSH setup pulls in my dotfiles stuff...
+      initExtra = ''
+        export powerline_config_path="${pkgs.python39Packages.powerline.outPath}/share/zsh/powerline.zsh"
+
+        if [ -f "$HOME/.config/nixpkgs/dotfiles/zsh/zshrc" ]; then
+          source "$HOME/.config/nixpkgs/dotfiles/zsh/zshrc"
+        fi
+      '';
+    };
+
+    alacritty = {
+      enable = true;
+    };
+
+    neovim = {
+      enable = true;
+      package = pkgs.neovim-nightly;
+      extraPackages = [ pkgs.gcc ];
+      viAlias = false;
+      extraConfig = builtins.readFile ../../nvim/init.vim;
+    };
+
+    git = {
+      enable = true;
+
+      userName = "Sam Martin-Brown";
+      userEmail = "Nivekkas@gmail.com";
+
+      delta.enable = true;
+      delta.options = {
+        line-numbers = true;
+      };
+
+      signing.signByDefault = true;
+      signing.key = "61CB737879759A958B6B886626E45D5144EF59EA";
+
+      aliases = {
+        d = "diff";
+      };
+
+      ignores = [
+        "tags"
+      ];
+    };
+  };
+
+  # Enable FZF
+  programs.fzf.enable = true;
+
+  # config file management
+  xdg.enable = true;
+  xdg.configFile = {
+    "alacritty/alacritty.yml".source = ../../dotfiles/alacritty/alacritty.yml;
+
+    "nvim/init.vim".source = ../../nvim/config/standalone.vim;
+    "nvim/after".source = ../../nvim/after;
+    "nvim/after".recursive = true;
+
+    "powerline".source = ../../dotfiles/powerline;
+  };
+}
+
+
---
+++ machines/mac-dev/default.nix
@@ -0,0 +1,32 @@
+# vim: set filetype=nix ts=2 sw=2 tw=0 et :
+{ pkgs, ...}:
+
+{
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+    }))
+  ];
+
+  imports = [
+    ../common
+  ];
+
+  # Various packages I want my user to have access to
+  home.packages = with pkgs; [
+    # TODO - setup Firefox via NUR or flakes using: https://github.com/toonn/nix-config/blob/master/darwin/apps/firefox/default.nix
+    # NOTE: Instructions related to NUR: https://github.com/nix-community/NUR#how-to-use
+  ];
+
+  # Setup any mac specific programs here...
+  programs = {
+  };
+
+  # File setup for various RC/Config files etc.
+  home.file = {
+    #".tmux.conf".source = ../../dotfiles/tmux/tmux.conf;
+    ".background-image".source = ../../backgrounds/pexels-pixabay-220072.jpg;
+    ".terminfo/61".source = pkgs.alacritty.terminfo.outPath + "/share/terminfo/61";
+    ".terminfo/a".source = pkgs.alacritty.terminfo.outPath + "/share/terminfo/a";
+  };
+}
