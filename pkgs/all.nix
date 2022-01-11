3c3135b568092a739f7eb939a80289f7c129d267
Sam Martin <Sam.Martin@F-Secure.com>
2022-01-11 21:11:40 +0000

    sort custom pkgs

--- pkgs/all.nix
+++ pkgs/all.nix
@@ -3,9 +3,9 @@
 
 {
   # awscli = pkgs.callPackage ./awscli { inherit pkgs; };
+  dunst-dracula-theme = pkgs.callPackage ./dunst-dracula-theme { inherit pkgs; };
+  i3-config = pkgs.callPackage ./i3-config { inherit pkgs; inherit python3; };
   polybar-launcher = pkgs.callPackage ./polybar-launcher { inherit pkgs; inherit python3; };
   polybar-spotify = pkgs.callPackage ./polybar-spotify { inherit pkgs; inherit python3; };
-  i3-config = pkgs.callPackage ./i3-config { inherit pkgs; inherit python3; };
-  dunst-dracula-theme = pkgs.callPackage ./dunst-dracula-theme { inherit pkgs; };
   rofi-dracula-theme = pkgs.callPackage ./rofi-dracula-theme { inherit pkgs; };
 }
