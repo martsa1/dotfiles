54717bd7f1bc2b62d5903fc9fc628ef9c1ce50be
Sam Martin <Sam.Martin@F-Secure.com>
2022-02-16 14:06:20 +0000

    Amend various laptop-specific settings, provide opengl wrapper tool.

--- machines/sm-fswbsk013.nix
+++ machines/sm-fswbsk013.nix
@@ -1,11 +1,28 @@
 { config, pkgs, ... }:
 
+let
+  glPkgs = import (<nixgl>) {};
+  #unstable_pkgs = import (
+  #  fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz
+  #) {};
+in
 {
   imports = [
     ./linux_base
   ];
+
   home.packages = with pkgs; [
     remmina
+    #evolutionWithPlugins
+    #evolution
+
+    #glPkgs.auto.nixGLDefault
+    glPkgs.nixGLIntel
+    #alacritty
+
+    openconnect
+
+    python3Packages.poetry
   ];
 
   # Seemingly needed for work machine to find all ZSH aliases, see here for more:
@@ -23,5 +40,29 @@
     options = [ "ctrl:nocaps" ];
     # To manually swap back to UK layout:
     # setxkbmap gb -option ctrl:nocaps (or run keyboard-gb)
+  };
+
+  # Rest of neovim settings are in common.
+  #programs.neovim = {
+  #plugins = with pkgs.vimPlugins; [
+  #  (nvim-treesitter.withPlugins (plugins: pkgs.tree-sitter.allGrammars))
+  #];
+  #};
+  # Try symlinking some package path over the top of old-style tree-sitter setup..?
+  xdg.configFile = {
+    #"nvim/init.vim".source = ../../nvim/config/standalone.vim;
+    #"nvim/after".source = ../../nvim/after;
+    #"nvim/after".recursive = true;
+
+    # If treesitter stuff needs rebuilding, it can be done safely inside a nix-shell:
+    # nix-shell -p neovim gcc coreutils git cacert --pure
+
+
+    #"nvim/plugged/nvim-treesitter/parser/python.so".source = #"${pkgs.tree-sitter-grammars.tree-sitter-python.outPath}/parser";
+    #"nvim/plugged/nvim-treesitter/parser/lua.so".source = "${pkgs.tree-sitter-grammars.tree-sitter-lua.outPath}/parser";
+    #"nvim/plugged/nvim-treesitter/parser/cpp.so".source = "${pkgs.tree-sitter-grammars.tree-sitter-cpp.outPath}/parser";
+    #"nvim/plugged/nvim-treesitter/parser/c.so".source = "${pkgs.tree-sitter-grammars.tree-sitter-c.outPath}/parser";
+    #"nvim/plugged/nvim-treesitter/parser/json.so".source = "${pkgs.tree-sitter-grammars.tree-sitter-json.outPath}/parser";
+    #"nvim/plugged/nvim-treesitter/parser/yaml.so".source = "${pkgs.tree-sitter-grammars.tree-sitter-yaml.outPath}/parser";
   };
 }
