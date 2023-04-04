021c90ffe1fbe85dab211b2d026256180c4d6aac
Sam Martin-Brown <Nivekkas@gmail.com>
2023-04-04 22:48:26 +0100

    Enable LSP Status in lua-line

--- nvim/lua/smb/statusline.lua
+++ nvim/lua/smb/statusline.lua
@@ -4,7 +4,13 @@ require("lualine").setup(
         sections = {
             lualine_a = {"mode"},
             lualine_b = {"branch", "diff", "diagnostics"},
-            lualine_c = {"filename", "lsp_progress"},
+            lualine_c = {
+                {
+                    "filename",
+                    path = 1
+                },
+                "lsp_progress"
+            },
             lualine_x = {"filetype", "encoding", "fileformat"},
             lualine_y = {"progress"},
             lualine_z = {"searchcount", "location"}
