3220ce8a382df9b36576be1853c4232e8bd6d8fb
Sam Martin-Brown <Nivekkas@gmail.com>
2023-04-15 14:44:13 +0000

    Merge some of old-laptop config into new laptop...

--- machines/sm-fswbsk088/default.nix
+++ machines/sm-fswbsk088/default.nix
@@ -1,21 +1,39 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
+let
+  glPkgs = import (<nixgl>) {};
+in
 {
   imports = [
     ../linux_base
   ];
 
   home.packages = with pkgs; [
+    #evolutionWithPlugins  # Can't use this outside of actual nixos it seems...
+    arandr
+    clang-tools  # C++ stuff for work...
+    deja-dup
+    duplicity
     ffmpeg
     font-manager
+    google-chrome
+    glPkgs.nixGLIntel
     globalprotect-openconnect
+    i3lock
+    kdiff3
+    less
     neovide
+    nodejs_latest # no getting away from node...
     openconnect
+    podman
     poetry
+    postman
     remmina
-    #rustdesk  - Seems broken at the moment.
+    remmina
+    vagrant
     virt-manager
     vlc
+    xdg-utils
   ];
 
   # Set a state version (determines various stateful bits and pieces. Should
@@ -34,6 +52,13 @@
     options = [ "ctrl:nocaps" ];
   };
 
+  # Attempt to ensure home-manager uses latest available nix version
+  nix.package = pkgs.nix;
+
+  home.shellAliases = {
+    docker = "podman";
+  };
+
   # Setup autorandr profiles
   programs.autorandr = {
     enable = true;
@@ -42,58 +67,28 @@
         "notify-i3" = "${pkgs.i3}/bin/i3-msg -s /run/user/1000/i3/ipc-socket.* restart && ${pkgs.systemd}/bin/systemctl --user restart polybar";
         "change-background" = "${pkgs.feh}/bin/feh --bg-scale ~/.background-image";
         "notify-user" = "${pkgs.libnotify}/bin/notify-send -i display 'Display profile changed:' $AUTORANDR_CURRENT_PROFILE";
-       # "change-dpi" = ''
-       #   case "$AUTORANDR_CURRENT_PROFILE" in
-       #     default)
-       #       DPI=120
-       #       ;;
-       #     home)
-       #       DPI=192
-       #       ;;
-       #     work)
-       #       DPI=144
-       #       ;;
-       #     *)
-       #       echo "Unknown profle: $AUTORANDR_CURRENT_PROFILE"
-       #       exit 1
-       #   esac
-       #   echo "Xft.dpi: $DPI" | ${pkgs.xorg.xrdb}/bin/xrdb -merge
-       #'';
-      };
-    };
-    profiles = {
-      "default" = {
-        fingerprint = {
-          eDP-1 = "00ffffffffffff004d10d01400000000031e0104b52215780ae668ab5035b8250c50540000000101010101010101010101010101010172e700a0f06045903020360050d21000001828b900a0f06045903020360050d210000018000000fe003930543032824c513135365231000000000002410332011200000b010a2020017502030f00e3058000e606050160602800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000aa";
-        };
-        config = {
-          eDP-1 = {
-            enable = true;
-            crtc = 0;
-            primary = true;
-            position = "0x0";
-            #mode = "3840x2400";
-            mode = "1920x1200";
-            rate = "59.88";
-            #rate = "59.99";
-            #transform = [
-            #  [ 0.5 0.0 0.0 ]
-            #  [ 0.0 0.5 0.0 ]
-            #  [ 0.0 0.0 1.0 ]
-            #];
-          };
-          DP-1.enable = false;
-          DP-2.enable = false;
-          DP-3.enable = false;
-        };
       };
     };
   };
-
 
   # Setup syncthing to pull over old machines stuff.
   services.syncthing = {
     enable = true;
     tray.enable = true;
+  };
+
+  # Seemingly needed for work machine to find all ZSH aliases, see here for more:
+  # https://github.com/nix-community/home-manager/issues/2562#issuecomment-1009381061
+  programs.zsh.initExtraBeforeCompInit = ''
+    fpath+=("${config.home.profileDirectory}"/share/zsh/site-functions "${config.home.profileDirectory}"/share/zsh/$ZSH_VERSION/functions "${config.home.profileDirectory}"/share/zsh/vendor-completions)
+  '';
+
+  # Do magic when not on NixOS
+  targets.genericLinux.enable = true;
+
+  services.picom = {
+    enable = true;
+    fade = true;
+    fadeDelta = 8;
   };
 }
