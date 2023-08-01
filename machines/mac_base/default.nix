27e651cef5bcc2fe9162797d6fb69c2d7e3278ab
Sam Martin-Brown <sam.martin@withsecure.com>
2023-08-01 17:17:52 +0100

    Remove duplicated nvim nightly overlay

--- machines/mac_base/default.nix
+++ machines/mac_base/default.nix
@@ -2,12 +2,6 @@
 { pkgs, ...}:
 
 {
-  nixpkgs.overlays = [
-    (import (builtins.fetchTarball {
-      url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
-    }))
-  ];
-
   imports = [
     ../common
   ];
