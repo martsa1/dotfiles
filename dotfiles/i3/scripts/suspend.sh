5487e79c08a9867071e4088c497478ad2541fc28
Sam Martin-Brown <Nivekkas@gmail.com>
2023-11-21 21:38:52 +0000

    Tweak suspend script to try and nuke Teams.

--- dotfiles/i3/scripts/suspend.sh
+++ dotfiles/i3/scripts/suspend.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-set +e
-set +x
+set -ex
 
 lock_script="$HOME/.config/home-manager/dotfiles/i3/scripts/lock.sh"
+
+vpn-down
+# Halt teams
+ps -elf | rg chrome | rg cifhbcnohmdccbgoicgdjpfamggdegmo | tr -s ' ' | cut -d ' ' -f 4 | xargs -r kill
 
 $lock_script
 
