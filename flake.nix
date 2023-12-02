a555fa24ce0fec1d2c1bfdf688f8bc46e574d2c7
Sam Martin-Brown <Nivekkas@gmail.com>
2023-12-02 11:16:17 +0000

    bump flake back to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    #nixpkgs.url = "github:nixos/nixpkgs/master";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -26,7 +27,7 @@
     homeConfigurations = {
       "sam@fswbsk088" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        modules = [./machines/fswbsk088/default.nix];
+        modules = [./machines/sm-fswbsk088/default.nix];
       };
 
       "sam@xps-laptop" = home-manager.lib.homeManagerConfiguration {
