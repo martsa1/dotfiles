e2c91ae52a3881f203f1a25e37b2eb19c6821af5
Sam Martin-Brown <Nivekkas@gmail.com>
2024-01-15 12:37:35 +0000

    Track nixpks-unstable rather than nixos-unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,8 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     #nixpkgs.url = "github:nixos/nixpkgs/master";
     home-manager = {
       url = "github:nix-community/home-manager";
