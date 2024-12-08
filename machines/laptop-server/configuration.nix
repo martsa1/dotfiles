7d2e699954f1052e12a27e6b83c585dc7b8e0b66
Sam Martin-Brown <Nivekkas@gmail.com>
2024-12-08 22:42:23 +0000

    Enable pwfeedback on homeserver

--- machines/laptop-server/configuration.nix
+++ machines/laptop-server/configuration.nix
@@ -103,6 +103,8 @@
     enable = true;
   };
 
+  security.sudo.extraConfig = "Defaults pwfeedback";
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
