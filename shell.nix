8a2dafa367f956715a338a816826111193852e8e
Sam Martin-Brown <samuel.martin-brown@watchtowr.com>
2025-10-07 21:20:48 +0800

    chore: provide pylsp & ruff in dotfiles shell.nix

--- shell.nix
+++ shell.nix
@@ -2,6 +2,8 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     alejandra
+    python3Packages.python-lsp-server
+    python3Packages.ruff
     # keep this line if you use bash
     # pkgs.bashInteractive
   ];
