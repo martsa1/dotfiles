96f322caa4b3cf2757c244f7862ee9dd786b6915
Sam <nivekkas@gmail.com>
2026-09-20 16:47:42 +0100

    drop long-obsolete poetry files

--- shell.nix
+++ shell.nix
@@ -2,10 +2,5 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     alejandra
-    python3Packages.python-lsp-server
-    python3Packages.ruff
-    uv
-    # keep this line if you use bash
-    # pkgs.bashInteractive
   ];
 }
